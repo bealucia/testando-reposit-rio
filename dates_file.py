a13478cf13886da308cf5f5ab325867646d102ec
MARIANA FERNANDES ROCHA <B51099@fgv.edu.br>
2023-09-18 09:08:17 -0300

    Comentários para opção 2

--- dates_file.py
+++ dates_file.py
@@ -1,10 +1,13 @@
+"""Módulo para arquivo de texto contendo as datas"""
 from datetime import date
 
 meses = {"Janeiro": 1, "Fevereiro": 2, "Março": 3, "Abril": 4, "Maio": 5, "Junho": 6, "Julho": 7, "Agosto": 8, "Setembro": 9, "Outubro": 10, "Novembro": 11, "Dezembro": 12 } 
 
 def option2():
+    # Arquivo contendo as datas
     arquivo = input("Insira o nome do arquivo: ")
 
+    # Lendo o arquivo
     with open (arquivo) as f:
         dates_list = f.readlines()
     
@@ -12,8 +15,10 @@ def option2():
 
     try:
         str_date = dates.split()
+
         str_date[2] = meses[str_date[2]]
         str_date[8] = meses[str_date[8]]
+        
         d0 = date(int(str_date[4]), int(str_date[2]),int(str_date[0]))
         d1 = date(int(str_date[10]), int(str_date[8]),int(str_date[6]))
 
