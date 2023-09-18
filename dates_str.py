9ed8cccc00cda9487ff7bd0b73a14c7107809555
PEDRO HENRIQUE COTERLI <B51063@fgv.edu.br>
2023-09-18 09:08:17 -0300

    Comentei o dates_str

--- dates_str.py
+++ dates_str.py
@@ -1,18 +1,28 @@
+"""Módulo contendo execução do aplicativo para a opção das datas em formato de string"""
+
 from datetime import date
 
+# Dicionário para substituir as strings dos meses por valores numéricos
 meses = {"Janeiro": 1, "Fevereiro": 2, "Março": 3, "Abril": 4, "Maio": 5, "Junho": 6, "Julho": 7, "Agosto": 8, "Setembro": 9, "Outubro": 10, "Novembro": 11, "Dezembro": 12 } 
 
-
 def option1():
-    dates = input("Escreva as datas por extenso:")
+    """Cálculo da diferença de dias com uma string das datas
+    """
+
+    dates = input("Escreva as datas por extenso: ")
+
     try:
         str_date = dates.split()
+        
+        # Convertendo os nomes dos meses para os valores numéricos correspondentes
         str_date[2] = meses[str_date[2]]
         str_date[8] = meses[str_date[8]]
+
+        # Criando as variáveis das datas com base nos valores da lista da string "splitada"
         d0 = date(int(str_date[4]), int(str_date[2]),int(str_date[0]))
         d1 = date(int(str_date[10]), int(str_date[8]),int(str_date[6]))
 
         print("Diferença de dias:", d1-d0)
 
     except:
-        print("A data precisa ser por extenso")
+        print("Valor inválido")
