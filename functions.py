bba86516a666d7bcafc7a70759c198c503846d31
MARIANA FERNANDES ROCHA <B51099@fgv.edu.br>
2023-09-18 08:31:09 -0300

    com tratamento

--- functions.py
+++ functions.py
@@ -1,12 +1,18 @@
 import datetime
-opcao = input("Opção 1: data por extenso;\nOpção 2: arquivo texto;\nOpção 3: sair da aplicação;\nOpç3ão escolhida:")
+opcao = input("Opção 1: data por extenso;\nOpção 2: arquivo texto;\nOpção 3: sair da aplicação;\nOpção escolhida:")
 
-if opcao == 1:
-    date = input("Escreva a data por extenso:")
-    try:
-        if not isinstance(date, str):
-            raise ValueError
-    except ValueError:
-        print("A data precisa ser por extenso")
-    else:
-        
+if opcao == 2:
+    print("Estou aqui")
+    date = input("Escreva as datas por extenso:")
+    print(date)
+    # meses = { "Janeiro": 1, "Fevereiro": 2, "Março": 3, "Abril": 4, "Maio": 5, "Junho": 6, "Julho": 7, "Agosto": 8, "Setembro": 9, "Outubro": 10, "Novembro": 11, "Dezembro": 12 } 
+    # try:
+    #     str_date = date.split()
+    #     str_date[2] = meses[str_date[2]]
+    #     str_date[8] = meses[str_date[8]]
+    #     d0 = date(str_date[4], str_date[2],str_date[0])
+    #     d1 = date(str_date[10], str_date[8],str_date[6])
+
+    #     print("Diferença de dias:", d1-d0)
+    # except ValueError:
+    #     print("A data precisa ser por extenso")       1
