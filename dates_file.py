afc2f766d1310da3816e1619adffe7024df6c8a2
PEDRO HENRIQUE COTERLI <B51063@fgv.edu.br>
2023-09-20 07:59:10 -0300

    Doctests no dates_file

--- dates_file.py
+++ dates_file.py
@@ -6,7 +6,33 @@ meses = {"Janeiro": 1, "Fevereiro": 2, "Março": 3, "Abril": 4, "Maio": 5, "Junh
 
 def option2():
     """Cálculo da diferença de dias a partir de um arquivo de texto
+
+    Teste normal com "Data.txt"
+    >>> option2()
+    Insira o nome do arquivo: 
+    Diferença de dias:  8410 days, 0:00:00
+
+    Teste com arquivo inexistente
+    >>> option2()
+    Insira o nome do arquivo: 
+    Arquivo não encontrado
+
+    Teste com arquivo com estrutura das datas errada ("Data_wrong_structure.txt")
+    >>> option2()
+    Insira o nome do arquivo: 
+    Estrutura das datas no arquivo inadequada
+
+    Teste com arquivo com data inexistente ("Data_inexistent.txt")
+    >>> option2()
+    Insira o nome do arquivo: 
+    Data não existente
+
+    Teste com arquivo com elementos faltantes nas datas ("Data_missing_elements.txt")
+    >>> option2()
+    Insira o nome do arquivo: 
+    Elementos faltantes na estrutura das datas
     """
+    
     # Arquivo contendo as datas
     arquivo = input("Insira o nome do arquivo: ")
 
@@ -20,6 +46,7 @@ def option2():
 
         str_date = dates.split()
 
+        # Substituindo os nomes dos meses pelos seus valores numéricos
         str_date[2] = meses[str_date[2]]
         str_date[8] = meses[str_date[8]]
         
@@ -28,13 +55,21 @@ def option2():
 
         print("\nDiferença de dias: ", d1-d0)
 
+    # Se o arquivo não existir
     except FileNotFoundError:
         print("\nArquivo não encontrado")
+    # Se as datas estiverem escritas de forma errada
     except KeyError:
         print("\nEstrutura das datas no arquivo inadequada")
+    # Se a data não existir (ex.: 32 de Janeiro de 2021)
     except ValueError:
         print("\nData não existente")
+    # Se faltarem elementos nas datas
     except IndexError:
         print("\nElementos faltantes na estrutura das datas")
+    # Outros erros
     except:
         print("\nErro desconhecido")
+
+if __name__ == "__main__":
+    doctest.testmod(verbose = True)
