06fb68e06dfae691304c612d73620685534c8ae8
Beatriz Lúcia Teixeira de Souza <125310668+bealucia@users.noreply.github.com>
2023-09-20 09:08:17 -0300

    Add files via upload

---
+++ test.unit.py
@@ -0,0 +1,17 @@
+import unittest 
+import dates_str as ds
+
+class TestDatesFile(unittest.TestCase):
+    
+        
+        
+class TestDatesConsole(unittest.TestCase):
+    def formato(self):
+        self.assertEqual(20 de junho de 2020, 30 de junho de 2020, 10)
+   def test_ano_bissexto(self):
+       self.assertEqual(ds.option1(27 de Fevereiro de 2016, 1 de Março de 2016), 3)
+            
+
+if __name__ == " __main__" :
+    unittest.main()
+    
